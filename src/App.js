90e54561790e980205337d7b7d6a9c27a442f916
marcelohcortez <marcelohcortez@gmail.com>
2022-12-19 15:53:16 +0000

    Add functions that hande cards check and winning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
+import { useState } from 'react'
+
 import './App.css';
 
 function App() {
+  const [grid, setGrid] = useState([
+    [0, 1, 2, 3],
+    [0, 1, 2, 3],
+    [0, 0, 0, 0]
+  ])
+
+  const [isRevealed, setIsRevealed] = useState(new Array(grid.length).fill('').map( () => 
+    new Array(grid[0].length).fill(false)
+  ))
+  
+  const [firstItem, setFirstItem] = useState()
+
+  function selectedCard(row, column) {
+    if (isRevealed[row][column]) return
+
+    const newIsRevealed = [...isRevealed]
+
+    newIsRevealed[row][column] = true
+    setIsRevealed(newIsRevealed)
+
+    if (firstItem) {
+      checkIfCardsAreEqual(newIsRevealed, row, column)
+    } else {
+      setFirstItem({row, column})
+    }   
+  }
+
+  function checkIfCardsAreEqual(newIsRevealed, row, column) {
+    const clickedCard = grid[row][column]
+    const firstCard = grid[firstItem.row][firstItem.column]
+
+    if (firstCard !== clickedCard) {
+      setTimeout(() => {
+        newIsRevealed[row][column] = false
+        newIsRevealed[firstItem.row][firstItem.column] = false
+        setIsRevealed([...newIsRevealed])
+      }, 1000)
+    } else {
+      const won = isRevealed.flat().every((state) => state)
+      if (won) {
+        setTimeout( () => {
+          alert('You won!')
+        }, 1000)
+      }
+    }
+    
+    setFirstItem(undefined)
+  }
+
   return (
     <div className="App">
-      
+      <div className='grid'>
+        {grid.map( (row, rowIndex) => (
+          <div className='row' key={rowIndex}>{row.map( (number, columnIndex) => (
+            <div className='card' key={columnIndex} 
+              onClick={ () => selectedCard(rowIndex, columnIndex)}>
+              {isRevealed[rowIndex][columnIndex] ? number : ''}
+            </div>
+          ))}</div>
+        ))}
+      </div>
     </div>
   );
 }
